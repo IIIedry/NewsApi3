afe6047dbc71e7d48d69d7ed5353644c035e5cb9
IIIedry <ilias.02.08@mail.ru>
2024-04-20 13:42:35 +0500

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,33 @@ plugins {
 }
 
 
+//allprojects.onEach { project ->
+//    project.afterEvaluate {
+//        with(project.plugins) {
+//            if (project.plugins.hasPlugin(libs.plugins.jetbrainsKotlinAndroid.get().pluginId)
+//                || project.plugins.hasPlugin(libs.plugins.jetbrainsKotlinJvm.get().pluginId)
+//            ) {
+//                project.plugins.apply(libs.plugins.detekt.get().pluginId)
+//
+//                project.extensions.configure<DetektExtension> {
+//                    config.setFrom(rootProject.files("default-detekt-config.yml"))
+//                }
+//
+//                project.dependencies.add("detektPlugins", libs.detekt.formatting.get().toString())
+//            }
+//        }
+//    }
+//}
+
 
 allprojects.onEach { project ->
+//    project.plugins.apply(libs.plugins.detekt.get().pluginId)
     project.afterEvaluate {
         with(project.plugins) {
-            if (project.plugins.hasPlugin(libs.plugins.jetbrainsKotlinAndroid.get().pluginId)
-                || project.plugins.hasPlugin(libs.plugins.jetbrainsKotlinJvm.get().pluginId)
+            if (hasPlugin(libs.plugins.jetbrainsKotlinAndroid.get().pluginId)
+                || hasPlugin(libs.plugins.jetbrainsKotlinJvm.get().pluginId)
             ) {
-                project.plugins.apply(libs.plugins.detekt.get().pluginId)
-
+                apply(libs.plugins.detekt.get().pluginId)
                 project.extensions.configure<DetektExtension> {
                     config.setFrom(rootProject.files("default-detekt-config.yml"))
                 }
